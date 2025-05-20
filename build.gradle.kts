34276af165fadec6a30e88774a06145af84e1c01
Said Oubella <saidooubella@gmail.com>
2025-05-20 05:33:20 +0100

    feat(build): generate an application when building with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    application
 }
 
 group = "io.github.saidooubella"
@@ -10,12 +11,16 @@ repositories {
 }
 
 kotlin {
-    jvmToolchain(11)
     explicitApi()
+    jvmToolchain(11)
     compilerOptions {
         freeCompilerArgs.add("-opt-in=kotlin.contracts.ExperimentalContracts")
         freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
     }
+}
+
+application {
+    mainClass = "io.github.saidooubella.sash.playground.MainKt"
 }
 
 dependencies {
@@ -27,3 +32,15 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.jar { manifest { attributes["Main-Class"] = application.mainClass } }
+
+tasks.distZip { archiveFileName = "sash.zip" }
+
+tasks.startScripts { enabled = false }
+
+tasks.classes { enabled = false }
+
+tasks.distTar { enabled = false }
+
+tasks.jar { enabled = false }
